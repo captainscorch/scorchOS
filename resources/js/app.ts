1ecd5ac1c028b007dde0340cb2dfa0d44a4dfa14
Daniel <daniel@unlimited.studio>
2025-08-09 14:44:52 +0200

    chore: update css, js config, add i18n

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,9 +4,15 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
+import { createI18n } from 'vue-i18n';
 import { ZiggyVue } from 'ziggy-js';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+});
+
+const appName = import.meta.env.VITE_APP_NAME || 'scorchOS';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
@@ -15,9 +21,23 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(i18n)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: '#2ab193',
     },
 });
+
+console.log('Like what you see? Connect with me at https://captainscor.ch 🚀');
+
+// Silence all console logs in production
+// if (import.meta.env.PROD) {
+//   console.log = () => {};
+//   console.debug = () => {};
+//   console.info = () => {};
+//   console.warn = () => {};
+//   console.error = () => {};
+// }
+
+// Designed & developed by captainscor.ch (https://captainscor.ch)
