f13c81637490d695bb93ed2ea668730798287765
Daniel <daniel@unlimited.studio>
2026-02-20 20:30:22 +0100

    fix: logo copy functionality

--- resources/js/components/CommandMenu.vue
+++ resources/js/components/CommandMenu.vue
@@ -85,7 +85,7 @@ const switchLanguage = (lang: string) => {
     setLocale(lang);
 };
 
-const copyToClipboard = async (text: string, type: 'Mark' | 'Logotype') => {
+const copyToClipboard = async (text: string, type: 'Logo') => {
     try {
         await navigator.clipboard.writeText(text);
         toast.success(t('commandMenu.toast.copied', { type }), {
@@ -96,7 +96,10 @@ const copyToClipboard = async (text: string, type: 'Mark' | 'Logotype') => {
     }
 };
 
-const markSvg = `<svg version="1.1" id="captainscorch_logo" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 56.2 46.5" xml:space="preserve"><style>.white{fill:#fff}</style><g id="XMLID_536_"><path id="XMLID_547_" class="white" d="M13.2 32.1c0 3.2 2.6 5.7 5.7 5.7h18.3c3.2 0 5.7-2.6 5.7-5.7V23H36v4c0 2.2-1.8 4-4 4H13.2v1.1z"/><path id="XMLID_541_" class="white" d="M42.9 13.8c0-3.1-2.6-5.7-5.7-5.7H19c-3.2 0-5.7 2.6-5.7 5.7v9.1h6.9v-4c0-2.2 1.8-4 4-4H43v-1.1z"/><path id="XMLID_540_" class="white" d="M25.8 19.7c-.5 0-.9.4-.9.9v4.6c0 .5.4.9.9.9h4.6c.5 0 .9-.4.9-.9v-4.6c0-.5-.4-.9-.9-.9h-4.6z"/><path id="XMLID_539_" class="white" d="M47.9 33.8c0 5.1-4.2 9.3-9.3 9.3H17.5c-5.1 0-9.3-4.2-9.3-9.3V30.9H5.6v2.9c0 6.6 5.3 11.9 11.9 11.9h21.2c6.6 0 11.9-5.3 11.9-11.9V22.9H48v10.9z"/><path id="XMLID_537_" class="white" d="M38.7.7H17.5C10.9.7 5.6 6 5.6 12.6v10.3h2.6V12.6c0-5.1 4.2-9.3 9.3-9.3h21.2c5.1 0 9.3 4.2 9.3 9.3V15h2.6v-2.3c0-6.6-5.4-12-11.9-12z"/></g></svg>`;
+const handleCopyMark = () => {
+    const markSvg = `<svg version="1.1" id="captainscorch_logo" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 56.2 46.5" xml:space="preserve"><style>.white{fill:#fff}</style><g id="XMLID_536_"><path id="XMLID_547_" class="white" d="M13.2 32.1c0 3.2 2.6 5.7 5.7 5.7h18.3c3.2 0 5.7-2.6 5.7-5.7V23H36v4c0 2.2-1.8 4-4 4H13.2v1.1z"/><path id="XMLID_541_" class="white" d="M42.9 13.8c0-3.1-2.6-5.7-5.7-5.7H19c-3.2 0-5.7 2.6-5.7 5.7v9.1h6.9v-4c0-2.2 1.8-4 4-4H43v-1.1z"/><path id="XMLID_540_" class="white" d="M25.8 19.7c-.5 0-.9.4-.9.9v4.6c0 .5.4.9.9.9h4.6c.5 0 .9-.4.9-.9v-4.6c0-.5-.4-.9-.9-.9h-4.6z"/><path id="XMLID_539_" class="white" d="M47.9 33.8c0 5.1-4.2 9.3-9.3 9.3H17.5c-5.1 0-9.3-4.2-9.3-9.3V30.9H5.6v2.9c0 6.6 5.3 11.9 11.9 11.9h21.2c6.6 0 11.9-5.3 11.9-11.9V22.9H48v10.9z"/><path id="XMLID_537_" class="white" d="M38.7.7H17.5C10.9.7 5.6 6 5.6 12.6v10.3h2.6V12.6c0-5.1 4.2-9.3 9.3-9.3h21.2c5.1 0 9.3 4.2 9.3 9.3V15h2.6v-2.3c0-6.6-5.4-12-11.9-12z"/></g></svg>`;
+    copyToClipboard(markSvg, 'Logo');
+};
 </script>
 
 <template>
@@ -204,7 +207,7 @@ const markSvg = `<svg version="1.1" id="captainscorch_logo" xmlns="http://www.w3
             </CommandGroup>
             <CommandSeparator />
             <CommandGroup :heading="t('commandMenu.groups.brandAssets')">
-                <CommandItem value="copy mark as svg" @select="copyToClipboard(markSvg, 'Mark')" class="group">
+                <CommandItem value="copy mark as svg" @select="handleCopyMark" class="group">
                     <svg
                         version="1.1"
                         id="Ebene_1"
